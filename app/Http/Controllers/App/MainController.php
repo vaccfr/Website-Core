cc33f812ef275fc62f9f13ed9bd37b563ca10e89
FrenchGap <peter.pare.new@gmail.com>
2020-07-13 09:37:18 +0300

    Some fooling around with apis and tables for now

--- app/Http/Controllers/App/MainController.php
+++ app/Http/Controllers/App/MainController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('app.index');
+        $cid = auth()->user()->vatsim_id;
+        try {
+            $response = (new Client)->get("https://api.vatsim.net/api/ratings/".$cid."/atcsessions", [
+                'headers' => [
+                    'Accepts' => 'application/json',
+                ]
+            ]);
+        } catch(ClientException $e) {
+            dd($e);
+        }
+        $response = json_decode((string) $response->getBody(), true);
+        $sessions = $response['results'];
+        // dd($sessions);
+        return view('app.index', [
+            'sessions' => $sessions,
+        ]);
     }
 }
--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
-@section('page-title', 'Home')
+@section('page-title')
+  Home | {{ Auth::user()->fname }}
+@endsection
 
 @section('page-header')
   <!-- Content Header (Page header) -->
@@ -8,7 +10,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Home page</h1>
+          <h1>Welcome back, {{ Auth::user()->fname }}!</h1>
         </div>
       </div>
     </div><!-- /.container-fluid -->
@@ -38,4 +40,29 @@
     <!-- /.card-footer-->
   </div>
   <!-- /.card -->
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Your last ATC sessions</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+      <table id="example2" class="table table-bordered table-hover">
+        <thead>
+        <tr>
+          <th>Callsign</th>
+          <th>Time spent</th>
+        </tr>
+        </thead>
+        <tbody>
+          @foreach ($sessions as $sess)
+            <tr>
+              <td>{{ $sess['callsign'] }}</td>
+              <td>{{ $sess['minutes_on_callsign'] }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!-- /.card-body -->
+  </div>
 @endsection
