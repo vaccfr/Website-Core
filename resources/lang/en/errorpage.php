c16e553e86c55e89c14c87c2b0686c638e218024
FrenchGap <peter.pare.new@gmail.com>
2020-07-13 18:56:53 +0300

    New error pages w/ translations

---
+++ resources/lang/en/errorpage.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+  '404_title' => 'Oops, the page you\'re <br />
+  looking for does not exist.',
+  '404_content' => 'You may want to head back to the homepage.<br />
+  If you think something is broken, report a problem.',
+  '404_goto_lp' => 'Go to homepage',
+  '404_report' => 'Report a problem',
+
+  '500_title' => 'Looks like we\'re having <br />
+  some server issues.',
+  '500_content' => 'Go back to the previous page and try again. If you think something is
+  broken, report a problem.',
+  '500_report' => 'Report a problem',
+
+  '503_title' => 'We\'re down for scheduled maintenance right now.',
+  '503_content' => 'We will back up very soon, sorry for the wait!',
+];
---
+++ resources/lang/fr/errorpage.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  '404_title' => 'Oops, la page que vous cherchez n\'existe pas',
+  '404_content' => 'Vous pouvez retourner à la page d\'accueil.<br />
+  Si vous pensez que c\'est une erreur, signalez-le nous',
+  '404_goto_lp' => 'Page d\'accueil',
+  '404_report' => 'Signaler un problème',
+
+  '500_title' => 'Une erreur du serveur est survenue.',
+  '500_content' => 'Vous pouvez retourner vers la page précedente, ou signaler ce problème',
+  '500_report' => 'Signaler un problème',
+
+  '503_title' => 'Le site est en maintenance.',
+  '503_content' => 'Nous serons bientôt de retour, merci pour votre patience!',
+];
