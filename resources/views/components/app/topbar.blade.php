a53be3440202729bf5e914daca84bf7d035b569a
FrenchGap <peter.pare.new@gmail.com>
2020-07-26 06:14:24 +0300

    Removed sidenav menu toggle

--- resources/views/components/app/topbar.blade.php
+++ resources/views/components/app/topbar.blade.php
@@ -2,9 +2,9 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
   <!-- Left navbar links -->
   <ul class="navbar-nav">
-    <li class="nav-item">
+    {{-- <li class="nav-item">
       <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
+    </li> --}}
     <li class="nav-item d-none d-sm-inline-block">
       <a href="#" class="nav-link">
         <span>
