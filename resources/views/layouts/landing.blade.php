6346a7818d39cc23fc7205ded8e6d7ecec260f38
FrenchGap <peter.pare.new@gmail.com>
2020-08-07 06:28:06 +0200

    Modified scrollbar on landing page

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -45,6 +45,38 @@
 
     <!-- Custom styles-->
     <link href="{{ asset('lp/css/main.css') }}" rel="stylesheet" />
+    <style type="text/css">
+      ::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+      }
+      ::-webkit-scrollbar-button {
+        width: 0px;
+        height: 0px;
+      }
+      ::-webkit-scrollbar-thumb {
+        background: #000000;
+        border: 0px none #ffffff;
+        border-radius: 0px;
+      }
+      ::-webkit-scrollbar-thumb:hover {
+        background: #343a40;
+      }
+      ::-webkit-scrollbar-thumb:active {
+        background: #000000;
+      }
+      ::-webkit-scrollbar-track {
+        background: #444a50;
+        border: 0px none #ffffff;
+        border-radius: 0px;
+      }
+      ::-webkit-scrollbar-track:active {
+        background: #333333;
+      }
+      ::-webkit-scrollbar-corner {
+        background: transparent;
+      }
+      </style>
   </head>
 
   <body>
