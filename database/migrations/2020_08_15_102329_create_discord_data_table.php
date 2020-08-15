a206b8e1e416fec209a49c7f1cbd58cf0d062fb6
FrenchGap <peter.pare.new@gmail.com>
2020-08-15 13:11:35 +0200

    Fixed it I guess

--- database/migrations/2020_08_15_102329_create_discord_data_table.php
+++ database/migrations/2020_08_15_102329_create_discord_data_table.php
@@ -14,6 +14,7 @@ class CreateDiscordDataTable extends Migration
     public function up()
     {
         Schema::create('discord_data', function (Blueprint $table) {
+            $table->id();
             $table->bigInteger('user_id')->unique();
             $table->bigInteger('discord_id');
             $table->string('username');
