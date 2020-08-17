99f7d6ee38f8b13f3b8d45dc7e169080be7146c8
FrenchGap <peter.pare.new@gmail.com>
2020-08-17 15:56:48 +0200

    Fixed some accent issues in ATIS

--- app/Http/Controllers/ATC/AtisController.php
+++ app/Http/Controllers/ATC/AtisController.php
@@ -218,8 +218,15 @@ class AtisController extends Controller
         return "Q F E ".$this->enunciate(str_pad($qnh-intval($elev/$this->feet_per_hpa), 4, "0", STR_PAD_LEFT));
     }
 
-    private function replaceAllAccents($input) {
-        return iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $input);
+    private function replaceAllAccents($str) {
+        $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+        $str = strtr( $str, $unwanted_array );
+
+        return $str;
     }
 
     private function makeClosedRWY($input)
