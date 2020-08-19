067ae38395fcc548153a08f6f44bb050dfc377e9
FrenchGap <peter.pare.new@gmail.com>
2020-08-19 22:54:54 +0200

    Added FR ATC Training + changed bg image

--- resources/views/landingpage/atc/training_fr.blade.php
+++ resources/views/landingpage/atc/training_fr.blade.php
@@ -7,7 +7,7 @@
 @section('page-masthead')
 <section class="intro">
   <div class="container_ATC">
-    <h1>Become ATC &darr;</h1>
+    <h1>Devenir contrôleur &darr;</h1>
   </div>
 </section>
 @endsection
@@ -17,77 +17,77 @@
   <ul>
     <li>
       <div class="ATC_TEXT">
-        <time>First Steps</time>
-        To start your ATC Training with VATFrance, you must first create a VATSIM account.
-        You can then use this account to <a href="{{ route('auth.login', ['locale' => app()->getLocale(), 'redirflag' => 'false']) }}" target="_blank" rel="noopener noreferrer">
-          log into your account</a> and request your mentoring.
+        <time>Premiers pas</time>
+        Pour devenir ATC chez VatFrance,vous devez d'abord vous crééer un compte VATSIM.
+        Vous pourrez alors utiliser ce compte <a href="{{ route('auth.login', ['locale' => app()->getLocale(), 'redirflag' => 'false']) }}" target="_blank" rel="noopener noreferrer">
+          pour vous connecter</a> et faire votre demande de mentoring.
       </div>
     </li>
     <li>
         <div class="ATC_TEXT">
-          <time>Initial Session</time>
-          Before you can start your training, you must first complete the initial information session. This session is organised once a month by our training staff
-          and takes place on our teamspeak server. 
-          During this session, you will learn basic information about the network and its organisation from an ATC perspective, and also how to use the main ATC tools.<br>
-          Some mentors are allowed to teach the contents of this information session alone, allowing you to start training right away.
+          <time>Session d'information</time>
+          Avant de commencer votre formation de contrôleur aérien virtuel, vous devez assister à une session d'information. Celle-ci est organisée une fois par mois par nos mentors
+          et a lieu sur notre serveur teamspeak. 
+          Pendant cette session, vous apprendrez les informations essentielles sur le réseau VATSIM, surtout son organisation du point de vue ATC mais aussi les outils que vous utiliserez pour votre formation.<br>
+          Certains mentors sont habilités à vous fournir ces informations d'eux-mêmes ce qui vous permettra de débuter votre formation de suite après.
         </div>
       </li>
       <li>
         <div class="ATC_TEXT">
           <time>Observation</time>
-          Before any concrete teaching takes place, you must complete at least 20 hours of observation time on the network.
-          This is accomplished using the tools you have been introduced to over the previous sessions.
-          The purpose of this observation time is for you to get acquainted with the network and the tools on your own, and get a better understanding of the processes which take place on the ATC side.
+          Avant de passer réellement à la pratique, vous devrez réaliser 20 heures d'observation sur le réseau.
+          Il s'agira pour vous de vous connecter sur le logiciel utilisé en tant que contrôleur pour observer vos futures actions.
+          L'objectif de ce temps d'observation est de vous permettre de vous familiariser avec le réseau et les outils par vous-même, et de mieux comprendre les processus qui se déroulent du côté ATC.
         </div>
       </li>
       <li>
         <div class="ATC_TEXT">
           <time>Training</time>
-          Your mentor will guide you, in most cases, throughout your entire ATC career in VATFrance - or up to the level they are allowed to teach. <br>
-          <b>Training can last between a few weeks to a few months for every single rank</b>. This time is necessary to guarantee the highest standard of teaching
-          and to ensure you will provide the highest possible quality service with the best confidence.
+          Le mentor qui vous aura été assigné vous suivra tout le long de votre carrière contrôleur VATFrance - ou jusqu'au niveau auquel il est apte de vous former. <br>
+          <b>La durée d'une formation est variable : ceci peu aller de quelques semaines à quelques mois.</b>. Ce temps est nécessaire pour garantir les meilleurs standards de formation
+          et pour nous assurer que vous pourrez proposer le meilleur service de contrôle possible.
         </div>
       </li>
       <li>
         <div class="ATC_TEXT">
-          <time>ATC Ranks Explained</time>
-          VATSIM ATCOs are awarded ratings based on their knowledge and progression.
-          These ranks are obtained through thorough teaching, learning and examination flows. <br>
-          The following ranks are available: <br>
-          - <b>S1</b> -> Student ATC 1, allowed (on authorisation) to control DEL and GND positions <br>
-          - <b>S2</b> -> Student ATC 2, allowed (on authorisation) to control TWR and below positions <br>
-          - <b>S3</b> -> Student ATC 3, allowed (on authorisation) to control APP, DEP and below positions <br>
-          - <b>C1</b> -> Controller 1, allowed to control CTR, FSS and below positions <br>
-          - <b>C3</b> -> Controller 3, a seniority rank with no further privileges. <br>
+          <time>Grades contrôleurs</time>
+          VATSIM reconnait les membres en leur délivrant des grades. Ces derniers témoignent uniquement de leurs connaissances et de leur progression.
+          Ces grades sont obtenus après plusieurs heures de formation, pratique, apprentissage et examens pour vérifier vos compétences. <br>
+          Voici ces grades: <br>
+          - <b>S1</b> -> Student ATC 1, permet (sur autorisation) de contrôler les positions DEL et GND <br>
+          - <b>S2</b> -> Student ATC 2, permet (sur autorisation) de contrôler les TWR et les positions précédentes <br>
+          - <b>S3</b> -> Student ATC 3, permet (sur autorisation) de contrôler APP, DEP et les positions précédentes <br>
+          - <b>C1</b> -> Controller 1, permet de contrôler les CTR, FSS et positions inférieures <br>
+          - <b>C3</b> -> Controller 3, un grade senior reconnaissant vos compétences, pas de privilèges particuliers. <br>
           <br>
-          <i>n.b.: some positions, airfields etc. may be restricted to ATCs based on various criteria. Please refer to staff for any questions.</i>
+          <i>n.b.: certaines positions, terrains etc peuvent être restreints aux ATCs ou soumis à une formation supplémentaire. Vous pouvez vous référer au staff pour plus de renseignements.</i>
         </div>
       </li>
       <li>
         <div class="ATC_TEXT">
-          <time>Examination & Progression Flow</time>
-          For every single rank from S1 to C1, the training is structured as follows: <br>
-          - Introductory session covering general and detailed concepts of the position <br>
-          - Preparation for theory exam <br>
-          - Theory exam <br>
-          - Assuming successful exam, start of the practical learning via simulation sessions <br>
-          - Following these sessions, the student will start training online with real VATSIM pilots <br>
-          - Once the student is deemed ready, they will take the practical exam (CPT) <br>
-          - If successful, the student is awarded the new rating <br>
+          <time>Examens & Progression dans le cursus</time>
+          Pour chacun des grades du S1 au C1, la formation est structurée comme suit: <br>
+          - Session d'introduction vous expliquant les informations de bases nécessaires pour suivre le cursus <br>
+          - Préparation à l'examen théorique <br>
+          - Examen théorique <br>
+          - Après réussite de l'examen, début des sessions de simulation <br>
+          - Après ces quelques sessions, l'élève commencera la pratique en réseau, avec les pilotes connectés sur VATSIM <br>
+          - Quand l'élève est prêt, le mentor demande l'examen pratique (CPT) pour son élève<br>
+          - Si réussite, l'élève est promu au nouveau grade <br>
           <br>
-          <i>n.b.: S1 are exempt from CPT and C3/i1/i3 are exempt from theory exams.</i>
+          <i>n.b.: Le S1 est exempté de CPT et les C3/I1/I3 sont exemptés d'examens théoriques.</i>
         </div>
       </li>
       <li>
         <div class="ATC_TEXT">
-          <time>Are you ready to take on the challenge?</time>
-          <a href="https://www.vatsim.net/join" target="_blank">Create your account</a>  today with VATSIM, then 
+          <time>Etes vous prêts à relever le défi ?</time>
+          <a href="https://www.vatsim.net/join" target="_blank">Créez votre compte</a>  aujourd'hui avec VATSIM
           <a href="{{ route('auth.login', ['locale' => app()->getLocale(), 'redirflag' => 'false']) }}" target="_blank" rel="noopener noreferrer">
-            click here
-          </a> to connect to your VATFrance account! <br>
-          Other links: <br>
-          - Our <a href="{{ route('discord.invite') }}" target="_blank">Discord Server</a> <br>
-          - Our <a href="{{ route('ts3.invite') }}">Teamspeak3 Server</a>
+            cliquez ici
+          </a> pour vous connecter sur votre compte VatFrance <br>
+          Autres liens <br>
+          - Notre <a href="{{ route('discord.invite') }}" target="_blank">serveur Discord</a> <br>
+          - Notre <a href="{{ route('ts3.invite') }}">serveur Teamspeak</a>
         </div>
       </li>
   </ul>
