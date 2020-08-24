6764e328e3f4787fb607a6a30bb7cd750792adef
FrenchGap <peter.pare.new@gmail.com>
2020-08-25 00:06:33 +0200

    UGH

--- app/Http/Controllers/ATC/BookingController.php
+++ app/Http/Controllers/ATC/BookingController.php
@@ -67,7 +67,6 @@ class BookingController extends Controller
 
         $bookings = Booking::where('vatsim_id', auth()->user()->vatsim_id)
         ->where('date', '>=', Carbon::now()->format('d.m.Y'))
-        ->where('start_time', '>=', Carbon::now()->format('H:i'))
         ->get();
         return view('app.atc.mybookings', [
             'positions' => $positions,
