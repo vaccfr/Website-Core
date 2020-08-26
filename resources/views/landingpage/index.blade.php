2f999f37e0667011a9545f4034c08c4ca2415a88
FrenchGap <peter.pare.new@gmail.com>
2020-08-26 18:50:29 +0200

    Fixed issue with video placement on new CSS

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -6,12 +6,12 @@
 
 @section('page-masthead')
 <header class="masthead">
-  {{-- <div>
+  <div>
       <video loop muted autoplay playsinline class="animated_bg__video" src="{{ asset('/media/video/lp/vatfrance_bg.mp4') }}">
           <source src="{{ asset('/media/video/lp/vatfrance_bg.webm') }}" type="video/webm">
           <source src="{{ asset('/media/video/lp/vatfrance_bg.mp4') }}" type="video/mp4">
       </video>
-  </div> --}}
+  </div>
   <div class="container-fluid h-100 bg-overlay d-flex align-items-end masthead-container" >
     <div class="row container-fluid no-gutters">
       <div class="col-12">
