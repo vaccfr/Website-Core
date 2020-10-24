19ef833cb904a54262f2cb5b973bf5393efbb647
FrenchGap <peter.pare.new@gmail.com>
2020-10-24 10:48:44 +0200

    Getting rid of resources + working initial version

--- resources/views/app/staff/calendar.blade.php
+++ resources/views/app/staff/calendar.blade.php
@@ -18,8 +18,8 @@
 @endsection
 
 @section('page-content')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-utilities@4.1.3/bootstrap-utilities.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.css" integrity="sha256-uq9PNlMzB+1h01Ij9cx7zeE2OR2pLAfRw3uUUOOPKdA=" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.js" integrity="sha256-mMw9aRRFx9TK/L0dn25GKxH/WH7rtFTp+P9Uma+2+zc=" crossorigin="anonymous"></script>
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-3">
@@ -37,59 +37,27 @@
     </div>
   </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.2/main.min.js"></script>
 <script>
-  var Calendar = FullCalendar.Calendar;
-  var calendarEl = document.getElementById('calendar');
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      firstDay: 1,
 
-  var calendar = new Calendar(calendarEl, {
-    events    : [
-      {
-        title          : 'All Day Event',
-        start          : new Date(y, m, 1),
-        backgroundColor: '#f56954', //red
-        borderColor    : '#f56954', //red
-        allDay         : true
-      },
-      {
-        title          : 'Long Event',
-        start          : new Date(y, m, d - 5),
-        end            : new Date(y, m, d - 2),
-        backgroundColor: '#f39c12', //yellow
-        borderColor    : '#f39c12' //yellow
-      },
-      {
-        title          : 'Meeting',
-        start          : new Date(y, m, d, 10, 30),
-        allDay         : false,
-        backgroundColor: '#0073b7', //Blue
-        borderColor    : '#0073b7' //Blue
-      },
-      {
-        title          : 'Lunch',
-        start          : new Date(y, m, d, 12, 0),
-        end            : new Date(y, m, d, 14, 0),
-        allDay         : false,
-        backgroundColor: '#00c0ef', //Info (aqua)
-        borderColor    : '#00c0ef' //Info (aqua)
-      },
-      {
-        title          : 'Birthday Party',
-        start          : new Date(y, m, d + 1, 19, 0),
-        end            : new Date(y, m, d + 1, 22, 30),
-        allDay         : false,
-        backgroundColor: '#00a65a', //Success (green)
-        borderColor    : '#00a65a' //Success (green)
-      },
-      {
-        title          : 'Click for Google',
-        start          : new Date(y, m, 28),
-        end            : new Date(y, m, 29),
-        url            : 'http://google.com/',
-        backgroundColor: '#3c8dbc', //Primary (light-blue)
-        borderColor    : '#3c8dbc' //Primary (light-blue)
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2020-10-24T08:00:00Z',
+          end: '2020-10-25T10:00:00Z',
+          uniqueID: 'TEST123'
+        },
+      ],
+      eventClick: function(info) {
+        console.log(info);
+        alert('Event: ' + info.event.extendedProps.uniqueID);
       }
-    ],
+    });
+    calendar.render();
   });
 </script>
 @endsection
